c57f6a5e5da880781311518e0e08e1f176da7413
410725008 <71903791+410725008@users.noreply.github.com>
2021-04-22 01:54:57 +0800

    Add files via upload

--- app/src/main/python/imageJAVA2PY.py
+++ app/src/main/python/imageJAVA2PY.py
@@ -4,7 +4,7 @@ def test_image(array,width,height):
     array = np.array(array,dtype='uint8')+12
     array_size = int(len(array)/4)
     array = array.reshape((height,width,4))[:,:,0:3]
-
+    # SHOW YOUR SKILL TO FUCK UP the FRAME
 
 
 
